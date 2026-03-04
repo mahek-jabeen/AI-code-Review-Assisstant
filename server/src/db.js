7923e4ef15dfc123a141a224d37bcfeaa5fd637f
mahekjabeen-07 <23951a054u@iare.ac.in>
2026-03-04 15:14:26 +0530

    fix mongodb atlas connection

--- server/src/db.js
+++ server/src/db.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ai_code_review");
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("✅ MongoDB connected successfully");
+    console.log("✅ MongoDB Atlas connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
